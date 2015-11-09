17832f6357055ab9bc6c6b6696a0a3f5ab37dc68
Simon N <simtuje@gmail.com>
2015-11-09 13:54:44 +0300

    GeoPosition sources formatted

--- src/main/java/com/goeuro/rest/GeoPosition.java
+++ src/main/java/com/goeuro/rest/GeoPosition.java
@@ -12,48 +12,45 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GeoPosition {
 
-	private Double latitude;
-	private Double longitude;
-	
-	/**
+    private Double latitude;
+    private Double longitude;
+
+    /**
      * Public default constructor
      */
-	public GeoPosition() {		
-	}
+    public GeoPosition() {		
+    }
 
-	/**
-	 * @return the latitude
-	 */
-	public Double getLatitude() {
-		return latitude;
-	}
+    /**
+     * @return the latitude
+     */
+    public Double getLatitude() {
+        return latitude;
+    }
 
-	/**
-	 * @param latitude the latitude to set
-	 */
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
 
-	/**
-	 * @return the longitude
-	 */
-	public Double getLongitude() {
-		return longitude;
-	}
+    /**
+     * @return the longitude
+     */
+    public Double getLongitude() {
+        return longitude;
+    }
 
-	/**
-	 * @param longitude the longitude to set
-	 */
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 
-	@Override
+    @Override
     public String toString() {
-        return "GeoPosition{" +
-                "latitude='" + latitude + '\'' +
-                ", longitude=" + longitude +
-                '}';
+        return "GeoPosition{" + "latitude='" + latitude + '\'' + ", longitude=" + longitude + '}';
     }
 }
