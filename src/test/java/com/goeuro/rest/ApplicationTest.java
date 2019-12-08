64660422afec9d2514e7c0f15b50be42adfaff9a
simonjenga <simtuje@gmail.com>
2019-12-08 21:55:00 +0300

    refactorings done to ApplicationTest class

--- src/test/java/com/goeuro/rest/ApplicationTest.java
+++ src/test/java/com/goeuro/rest/ApplicationTest.java
@@ -23,10 +23,10 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @ContextConfiguration(classes = Application.class, loader = AnnotationConfigContextLoader.class)
 public class ApplicationTest {
 
-	@Autowired
+    @Autowired
     private ApplicationContext ctx;
 
-	/**
+    /**
      * Test valid city name using {@code ApplicationContext} and {@code CommandLineRunner}.
      * The results for this test are logged to console.
      * 
@@ -37,58 +37,58 @@ public class ApplicationTest {
         CommandLineRunner runner = this.ctx.getBean(CommandLineRunner.class);
         runner.run("Melbourne");
     }
-    
+
     /**
      * Test for an Empty city name.
      * The results for this test are logged to console.
      * 
      * @throws Exception If some problem inside
      */
-    @Ignore
+    @Test
     public void testEmptyCityName() throws Exception {
         Application.main(new String[] { "" });
     }
-	
-	/**
+
+    /**
      * Test for a Valid city name.
      * The results for this test are logged to console.
      * 
      * @throws Exception If some problem inside
      */
-    @Ignore
+    @Test
     public void testValidCityName() throws Exception {
         Application.main(new String[] { "New", "York" });
     }
-	
+
     /**
      * Test for an Invalid city name.
      * The results for this test are logged to console.
      * 
      * @throws Exception If some problem inside
      */
-    @Ignore
+    @Test
     public void testInValidCityName() throws Exception {
         Application.main(new String[] { "@#$?x" });
     }
-	
+
     /**
      * Test for an Valid city name with invalid data among the names provided.
      * The results for this test are logged to console.
      * 
      * @throws Exception If some problem inside
      */
-    @Ignore
+    @Test
     public void testValidAndInvalidInputArguments() throws Exception {
         Application.main(new String[] { "Rio", "de", null, "Janeiro", null });
     }
-	
+
     /**
      * Test for mandatory and valid input arguments otherwise, the validation
      * mechanism with throw an {@link java.lang.IllegalArgumentException)}
      * 
      * @throws Exception If some problem inside
      */
-    @Ignore
+    @Test
     public void testInvalidInputArguments() throws Exception {
         try {
             Application.main(new String[] { "", null });
